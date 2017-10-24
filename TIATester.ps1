d6fdf5e33e789d7d20a7811a7b154034631282c4
James <james.l.smith@pioneer.com>
2017-10-23 20:58:59 -0500

    Added first 200 results runs

---
+++ TIATester.ps1
@@ -0,0 +1,29 @@
+﻿function CloneGitRepo {
+    param([string] $gitURI, [string] $gitSrcDir)
+    $projectCloned = Test-Path $solutionUnderTestPath 
+    if (-Not $projectCloned) {
+        echo "Cloning the project under test git repo"
+        git clone $solutionUnderTestGitHTTTPS "$gitSrcDir\$solutionName"
+    }
+}
+
+function ResetGitRepo {
+    git fetch origin
+    git reset --hard origin/master
+    git clean -xdff -e **/*/storage.ide
+}
+
+function ReverGitRepoXNumberOfCommintsBack {
+    param([string] $gitRepoPath, [int] $numberOfCommitsBack)
+    $workingDir = pwd
+    cd $gitRepoPath
+    ResetGitRepo
+    git reset --hard HEAD~$numberOfCommitsBack
+    git clean -xdff -e **/*/storage.ide
+    cd $workingDir
+} 
+
+function CopyGitHubRepoToTFSGitRepo{
+    param([string] $gitHubLocalPath, [string] $tfsRepoLocalPath)
+
+}
