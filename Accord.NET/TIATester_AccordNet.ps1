148ee68f7c9b84da3292236563e90164a681fe00
James <james.l.smith@pioneer.com>
2017-11-30 13:44:32 -0600

    Updates to VSTS run scripts

---
+++ Accord.NET/TIATester_AccordNet.ps1
@@ -0,0 +1,83 @@
+﻿$rootTestDir="/Users/smithj8-mac/uiucGitHub"
+$tiaGitSolutionDirectory="$rootTestDir/TestProjectsVSTS/Accord.NET"
+$gitHubSolutionDirectory="$rootTestDir/TestProjects/framework"
+$gitHubDirectory="$rootTestDir/TestProjects"
+$gitHubSrcURI="https://github.com/accord-net/framework.git"
+$commitToAnalyze=500
+$numberOfCommitsToAnalyze=1
+
+function CloneGitRepo {
+    param([string] $gitURI, [string] $gitSrcDir)
+    $projectCloned = Test-Path $gitHubSolutionDirectory 
+    if (-Not $projectCloned) {
+        echo "Cloning the project under test git repo"
+        git clone $gitURI "$gitSrcDir"
+    }
+}
+
+function ResetGitRepo {
+    git fetch origin
+    git reset --hard origin/master
+    git clean -xdff -e **/*/storage.ide
+}
+
+function CommitVSTSGitChanges {
+    param([string] $msg)
+    git add -A
+    git commit -m $msg
+}
+
+function PushVSTSGitChanges {
+    param([string] $msg)
+    $workingDir = pwd
+    cd $tiaGitSolutionDirectory
+    CommitVSTSGitChanges -msg $msg
+    git push
+    cd $workingDir
+}
+
+function ReverGitRepoXNumberOfCommintsBack {
+    param([string] $gitRepoPath, [int] $numberOfCommitsBack)
+    $workingDir = pwd
+    cd $gitRepoPath
+    ResetGitRepo
+    git reset --hard HEAD~$numberOfCommitsBack
+    git clean -xdff -e **/*/storage.ide
+    cd $workingDir
+} 
+
+function CleanVSTSDirectory {
+    $vstsFolderExists = Test-Path $tiaGitSolutionDirectory 
+    if ($vstsFolderExists) {
+        $items= Get-ChildItem -Path  $tiaGitSolutionDirectory -Recurse  |
+        Select -ExpandProperty FullName |
+        Where {$_ -notlike '*TestAdaptors*'} |
+        sort-object length -Descending |
+        Remove-Item -Force -Recurse
+    }
+}
+
+function CopyGitHubSrcToVSTSGitRepo {
+    Copy-Item "$gitHubSolutionDirectory\*" $tiaGitSolutionDirectory -Recurse
+}
+
+CloneGitRepo -gitURI $gitHubSrcURI -gitSrcDir $gitHubSolutionDirectory 
+
+$numberOfCommitsLeft=$numberOfCommitsToAnalyze
+while ($numberOfCommitsLeft -gt 0) {
+    #Checkout git commit i commits ago
+    $currentCommit=$commitToAnalyze-($numberOfCommitsToAnalyze - $numberOfCommitsLeft)
+    ReverGitRepoXNumberOfCommintsBack -gitRepoPath $gitHubSolutionDirectory -numberOfCommitsBack $currentCommit
+
+    #Clean the VSTS git directory to copy into
+    CleanVSTSDirectory
+
+    #Copy the Source GitHub Repo to VSTS Git Repo
+     CopyGitHubSrcToVSTSGitRepo
+
+    # #Push the new changeset to VSTS
+     PushVSTSGitChanges -m "$currentCommit commits back from current GitHub head"
+
+     $numberOfCommitsLeft -= 1
+     Start-Sleep -s 30
+}
---
+++ OptiKey/TIATester_OptiKey.ps1
@@ -0,0 +1,79 @@
+﻿$tiaGitSolutionDirectory="D:\UIUC-GIT\TestProjectSourceVSTS\OptiKey"
+$gitHubSolutionDirectory="D:\UIUC-GIT\TestProjectsGitHubSourceTIA\OptiKey"
+$gitHubDirectory="D:\UIUC-GIT\TestProjectsGitHubSourceTIA"
+$gitHubSrcURI="https://github.com/OptiKey/OptiKey.git"
+$commitToAnalyze=500
+$numberOfCommitsToAnalyze=200
+
+function CloneGitRepo {
+    param([string] $gitURI, [string] $gitSrcDir)
+    $projectCloned = Test-Path $gitHubSolutionDirectory 
+    if (-Not $projectCloned) {
+        echo "Cloning the project under test git repo"
+        git clone $gitURI "$gitSrcDir"
+    }
+}
+
+function ResetGitRepo {
+    git fetch origin
+    git reset --hard origin/master
+    git clean -xdff -e **/*/storage.ide
+}
+
+function CommitVSTSGitChanges {
+    param([string] $msg)
+    git add -A
+    git commit -m $msg
+}
+
+function PushVSTSGitChanges {
+    param([string] $msg)
+    $workingDir = pwd
+    cd $tiaGitSolutionDirectory
+    CommitVSTSGitChanges -msg $msg
+    git push
+    cd $workingDir
+}
+
+function ReverGitRepoXNumberOfCommintsBack {
+    param([string] $gitRepoPath, [int] $numberOfCommitsBack)
+    $workingDir = pwd
+    cd $gitRepoPath
+    ResetGitRepo
+    git reset --hard HEAD~$numberOfCommitsBack
+    git clean -xdff -e **/*/storage.ide
+    cd $workingDir
+} 
+
+function CleanGitDirectory {
+    $items= Get-ChildItem -Path  $tiaGitSolutionDirectory -Recurse  |
+    Select -ExpandProperty FullName |
+    Where {$_ -notlike '*TestAdaptors*'} |
+    sort length -Descending |
+    Remove-Item -Force
+}
+
+function CopyGitHubSrcToVSTSGitRepo {
+    Copy-Item "$gitHubSolutionDirectory\*" $tiaGitSolutionDirectory -Recurse
+}
+
+CloneGitRepo -gitURI $gitHubSrcURI -gitSrcDir $gitHubSolutionDirectory 
+
+$numberOfCommitsLeft=$numberOfCommitsToAnalyze
+while ($numberOfCommitsLeft -gt 0) {
+    #Checkout git commit i commits ago
+    $currentCommit=$commitToAnalyze-($numberOfCommitsToAnalyze - $numberOfCommitsLeft)
+    ReverGitRepoXNumberOfCommintsBack -gitRepoPath $gitHubSolutionDirectory -numberOfCommitsBack $currentCommit
+
+    #Clean the VSTS git directory to copy into
+    CleanGitDirectory
+
+    #Copy the Source GitHub Repo to VSTS Git Repo
+    CopyGitHubSrcToVSTSGitRepo
+
+    #Push the new changeset to VSTS
+    PushVSTSGitChanges -m "$currentCommit commits back from current GitHub head"
+
+    $numberOfCommitsLeft -= 1
+    Start-Sleep -s 30
+}
